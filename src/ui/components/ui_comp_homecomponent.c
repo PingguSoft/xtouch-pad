adddbfc6b37606fcff792306db17750cdc9680d4
PingguSoft <pinggusoft@gmail.com>
2024-02-13 03:35:22 +0900

    change pixel to percentage

--- src/ui/components/ui_comp_homecomponent.c
+++ src/ui/components/ui_comp_homecomponent.c
@@ -348,7 +348,7 @@ lv_obj_t *ui_homeComponent_create(lv_obj_t *comp_parent)
 
     lv_obj_t *cui_mainScreenStatusBar;
     cui_mainScreenStatusBar = lv_obj_create(cui_mainScreenLeft);
-    lv_obj_set_height(cui_mainScreenStatusBar, 28);
+    lv_obj_set_height(cui_mainScreenStatusBar, lv_pct(20));
     lv_obj_set_width(cui_mainScreenStatusBar, lv_pct(100));
     lv_obj_set_x(cui_mainScreenStatusBar, 386);
     lv_obj_set_y(cui_mainScreenStatusBar, 178);
@@ -810,6 +810,7 @@ lv_obj_t *ui_homeComponent_create(lv_obj_t *comp_parent)
     lv_obj_t *cui_mainScreenLightButton;
     cui_mainScreenLightButton = lv_obj_create(cui_mainScreenRight);
     lv_obj_set_width(cui_mainScreenLightButton, lv_pct(100));
+    lv_obj_set_height(cui_mainScreenLightButton, lv_pct(30));
     lv_obj_set_flex_grow(cui_mainScreenLightButton, 1);
     lv_obj_set_x(cui_mainScreenLightButton, 386);
     lv_obj_set_y(cui_mainScreenLightButton, 178);
@@ -847,7 +848,7 @@ lv_obj_t *ui_homeComponent_create(lv_obj_t *comp_parent)
 
     lv_obj_t *cui_mainScreenTemps;
     cui_mainScreenTemps = lv_obj_create(cui_mainScreenRight);
-    lv_obj_set_height(cui_mainScreenTemps, 180);
+    lv_obj_set_height(cui_mainScreenTemps, lv_pct(70));
     lv_obj_set_width(cui_mainScreenTemps, lv_pct(100));
     lv_obj_set_x(cui_mainScreenTemps, 386);
     lv_obj_set_y(cui_mainScreenTemps, 178);
